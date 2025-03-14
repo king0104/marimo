f56d643440981a88098a3d637700423fd9c0ea6b
king0104 <king0104@users.noreply.github.com>
2025-03-14 11:08:06 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/ssafy/marimo/MarimoApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.marimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/marimo/MarimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.marimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
