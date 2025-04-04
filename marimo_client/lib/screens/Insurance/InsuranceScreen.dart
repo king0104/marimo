478bfb666b965248ef8d6726cf3ec1ff2c7dffa8
DESKTOP-4QCTMKG\SSAFY <tmddnjs1447@naver.com>
2025-04-04 10:27:01 +0900

    ✨feat: 마이 - 보험 등록 및 마일리지 특약 관리 새 UI 생성

--- marimo_client/lib/screens/Insurance/InsuranceScreen.dart
+++ marimo_client/lib/screens/Insurance/InsuranceScreen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:marimo_client/screens/Insurance/widgets/InsuranceHeader.dart';
-import 'package:marimo_client/screens/Insurance/widgets/InsurancePageView.dart';
 import 'package:marimo_client/commons/CustomAppHeader.dart';
+import 'package:marimo_client/screens/Insurance/widgets/InsuranceHeader.dart';
+import 'package:marimo_client/screens/Insurance/widgets/InsuranceDetails.dart';
+import 'package:marimo_client/screens/Insurance/widgets/InsurancePageView.dart';
 import 'package:provider/provider.dart';
 import 'package:marimo_client/providers/navigation_provider.dart';
 
@@ -14,8 +15,19 @@ class InsuranceScreen extends StatelessWidget {
     this.isInsuranceRegistered = false,
   });
 
+  // 만기일까지 남은 일수 계산 함수 추가
+  bool _isNearExpiration(String expiryDate) {
+    final expiry = DateTime.parse(expiryDate.replaceAll('.', '-'));
+    final today = DateTime.now();
+    final difference = expiry.difference(today).inDays;
+    return difference <= 30 && difference >= 0;  // 만기 30일 이내이고 아직 만기가 지나지 않은 경우
+  }
+
   @override
   Widget build(BuildContext context) {
+    // 만기일 체크 (InsuranceDetails의 '2024.12.31' 형식 사용)
+    final bool showExpiryNotice = _isNearExpiration('2025.04.31');
+
     return Scaffold(
       backgroundColor: const Color(0xFFFBFBFB),
       appBar: CustomAppHeader(
@@ -43,18 +55,120 @@ class InsuranceScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                '내 자동차 보험 관리',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
-                ),
+              SizedBox(height: 10.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    isInsuranceRegistered 
+                      ? '내 자동차 마일리지 특약 관리'
+                      : '내 자동차 보험 관리',
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  if (isInsuranceRegistered)
+                    Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xFF000000).withOpacity(0.02),
+                            blurRadius: 11.8,
+                            spreadRadius: 4,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: OutlinedButton(
+                        onPressed: () {
+                          // TODO: 삭제 기능 구현
+                        },
+                        style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 6.h),
+                          backgroundColor: Colors.white,
+                          side: BorderSide(
+                            color: const Color(0xFF000000).withOpacity(0.2),
+                            width: 0.2,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: Text(
+                          '삭제하기',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
               ),
-              SizedBox(height: 20.h),
-              if (isInsuranceRegistered)
-                const InsuranceHeader()
-              else
+              if (isInsuranceRegistered && showExpiryNotice) ...[  // 조건 추가
+                SizedBox(height: 16.h),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFEFEF),
+                    borderRadius: BorderRadius.circular(8.r),
+                    border: Border.all(
+                      color: const Color(0xFFFF9898),
+                      width: 1,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.receipt_long,
+                        size: 20.w,
+                        color: const Color(0xFFFF9898),
+                      ),
+                      SizedBox(width: 8.w),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '이번 달에 보험이 만기되어요.',
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: const Color(0xFF000000),
+                              ),
+                            ),
+                            Text(
+                              '지금 주행거리를 제출하는게 유리해요!',
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                color: const Color(0xFF000000),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+              SizedBox(height: 10.h),
+              if (isInsuranceRegistered) ...[
+                const InsuranceHeader(),
+                SizedBox(height: 20.h),
+                Text(
+                  '상세 정보',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                const InsuranceDetails(),
+                SizedBox(height: 40.h),
+              ] else
                 const InsurancePageView(),
             ],
           ),
--- marimo_client/lib/screens/Insurance/widgets/InsuranceDetails.dart
+++ marimo_client/lib/screens/Insurance/widgets/InsuranceDetails.dart
@@ -23,15 +23,15 @@ class InsuranceDetails extends StatelessWidget {
         children: [
           _buildDetailRow('최초 주행거리 등록일', '2024.01.01'),
           SizedBox(height: 16.h),
-          _buildDetailRow('등록 최초 주행거리', '21,000km'),
+          _buildDetailRow('보험 만기일', '2024.12.31'),
+          SizedBox(height: 16.h),
+          _buildDetailRow('최초 등록 주행거리', '21,000km'),
           SizedBox(height: 16.h),
           _buildDetailRow('현재 총 주행거리', '24,600km'),
           SizedBox(height: 16.h),
           _buildDetailRow('계산 주행거리', '3,600km'),
           SizedBox(height: 16.h),
           _buildDetailRow('월 평균 환산 주행거리', '30km/월'),
-          SizedBox(height: 24.h),
-          _buildTipSection(),
         ],
       ),
     );
@@ -45,50 +45,15 @@ class InsuranceDetails extends StatelessWidget {
           label,
           style: TextStyle(
             fontSize: 14.sp,
-            color: Colors.grey[600],
+            fontWeight: FontWeight.w500,
           ),
         ),
         Text(
           value,
           style: TextStyle(
             fontSize: 14.sp,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ],
-    );
-  }
 
-  Widget _buildTipSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Icon(Icons.lightbulb_outline, size: 20.w, color: Colors.amber),
-            SizedBox(width: 8.w),
-            Text(
-              '관리 팁',
-              style: TextStyle(
-                fontSize: 14.sp,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 8.h),
-        Container(
-          padding: EdgeInsets.all(12.w),
-          decoration: BoxDecoration(
-            color: const Color(0xFFFFEFEF),
-            borderRadius: BorderRadius.circular(8.r),
-          ),
-          child: Text(
-            '400km 이상 운행하면 할인율이 낮아져요!',
-            style: TextStyle(
-              fontSize: 14.sp,
-              color: Colors.red[400],
-            ),
+            color: Colors.grey[600],
           ),
         ),
       ],
