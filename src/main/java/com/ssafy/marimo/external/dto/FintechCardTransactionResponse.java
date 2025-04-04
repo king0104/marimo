2952d8ca64d7ec6bd17e096a5ff7f16433077c62
king0104 <king0104@users.noreply.github.com>
2025-04-04 17:15:31 +0900

    chore: 브랜치 변경

--- src/main/java/com/ssafy/marimo/external/dto/FintechCardTransactionResponse.java
+++ src/main/java/com/ssafy/marimo/external/dto/FintechCardTransactionResponse.java
@@ -3,25 +3,55 @@ package com.ssafy.marimo.external.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class FintechCardTransactionResponse {
 
     @JsonProperty("Header")
     private Header header;
 
-    // 추후 거래 내역 리스트가 추가되면 여기에 추가
-    // private List<Transaction> transactions;
+    @JsonProperty("REC")
+    private Rec rec;
 
     @Data
     public static class Header {
+        private String responseCode;
+        private String responseMessage;
         private String apiName;
         private String transmissionDate;
         private String transmissionTime;
         private String institutionCode;
-        private String fintechAppNo;
+        private String apiKey;
         private String apiServiceCode;
         private String institutionTransactionUniqueNo;
-        private String apiKey;
-        private String userKey;
+    }
+
+    @Data
+    public static class Rec {
+        private String cardIssuerCode;
+        private String cardIssuerName;
+        private String cardName;
+        private String cardNo;
+        private String cvc;
+        private String estimatedBalance;
+
+        @JsonProperty("transactionList")
+        private List<Transaction> transactionList;
+    }
+
+    @Data
+    public static class Transaction {
+        private String transactionUniqueNo;
+        private String categoryId;
+        private String categoryName;
+        private String merchantId;
+        private String merchantName;
+        private String transactionDate;
+        private String transactionTime;
+        private String transactionBalance;
+        private String cardStatus; // e.g. 승인, 취소
+        private String billStatementsYn; // e.g. Y/N
+        private String billStatementsStatus; // e.g. 결제, 미결제
     }
 }
