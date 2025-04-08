c2cd87b67bd2599c6a97882f08ac52de4095e7b7
king0104 <king0104@users.noreply.github.com>
2025-04-08 15:14:25 +0900

    fix: 차계부 전체 조회 엔드포인트 수정

--- src/main/java/com/ssafy/marimo/payment/controller/PaymentController.java
+++ src/main/java/com/ssafy/marimo/payment/controller/PaymentController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,9 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @GetMapping
+    @GetMapping("/cars/{carId}")
     public ResponseEntity<GetMonthlyPaymentResponse> getMonthlyHistory(
-            @RequestParam("carId") @DecryptedId Integer carId,
+            @PathVariable("carId") @DecryptedId Integer carId,
             @RequestParam("year") int year,
             @RequestParam("month") int month
     ) {
